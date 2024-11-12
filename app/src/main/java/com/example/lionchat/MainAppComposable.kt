30f250a6433ea168d37cf5f154117e543f43703d
Artem Volk <artem_volk04@mail.ru>
2024-11-12 10:22:12 +0500

    Исправил небольшие баги

--- app/src/main/java/com/example/lionchat/MainAppComposable.kt
+++ app/src/main/java/com/example/lionchat/MainAppComposable.kt
@@ -4,11 +4,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.lionchat.feature.auth.signin.SignInScreen
 import com.example.lionchat.feature.auth.signup.SignUpScreen
+import com.example.lionchat.feature.chat.ChatScreen
 import com.example.lionchat.feature.home.HomeScreen
 import com.google.firebase.auth.FirebaseAuth
 
@@ -27,6 +30,14 @@ fun MainApp() {
             }
             composable("home") {
                 HomeScreen(navController)
+            }
+            composable("chat/{channelId}", arguments = listOf(
+                navArgument("channelId") {
+                    type = NavType.StringType
+                }
+            )) {
+                val channelId = it.arguments?.getString("channelId") ?: ""
+                ChatScreen(navController, channelId)
             }
         }
     }
